6e257d1e6664da952362ac38e4b5522918ae0172
DarkN1ght009 <savyabang@gmail.com>
2025-05-03 12:28:09 +0530

    Add files via upload

--- birthday.py
+++ birthday.py
@@ -1,1 +1,11 @@
-ram = 29 august 
+birthday1 = "January 5, 2012"
+birthday2 = "March 12, 2012"
+birthday3 = "July 23, 2012"
+birthday4 = "October 8, 2012"
+birthday5 = "December 31, "
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
