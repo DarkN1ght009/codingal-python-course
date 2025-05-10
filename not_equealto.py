f81283d7214d0d1253204b55ec40573b38a7a478
DarkN1ght009 <savyabang@gmail.com>
2025-05-10 15:46:13 +0530

    Add files via upload

---
+++ not_equealto.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+if (num1!=num2) :
+    print ("The first and second number are not equal")
+else :
+    print ("The numbers are equal")
+
+
+a = 'python'
+b = "bark"
+
+if (a!=b) :
+    print ('They are different')
+else :
+    print ("They are same")
+
+a = 2
+b = 3
+
+if ((a==2)!=(b==3)) :
+    print ("Hello")
+else :
+    print ("Bye Bye")
