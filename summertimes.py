07c475e528b3545d24180bc96f64e43bbcfa01dd
DarkN1ght009 <savyabang@gmail.com>
2025-05-11 16:52:04 +0530

    Add files via upload

---
+++ summertimes.py
@@ -0,0 +1,6 @@
+temp = int(input ("Enter the  current temperature of your region in celcius : "))
+
+if (temp > 25 ) :
+    print ("You should wear light clothes")
+else :
+    print ("You should not wear light clothes")
