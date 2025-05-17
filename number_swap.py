ea5ce4be2abed98a3f134bbf1b0b2525f6854df6
DarkN1ght009 <savyabang@gmail.com>
2025-05-17 15:50:54 +0530

    Add files via upload

---
+++ number_swap.py
@@ -0,0 +1,8 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+c = int(input("Enter a number : "))
+
+print ("I have swaped the values for the numbers ")
+print ("The new value for the first number number is  ", b)
+print ("The new value for the second number number is  ", c)
+print ("The new value for the third number number is  ", a)
